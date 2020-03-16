f33b78023229c2f465ddea877b7ddf0cb5b12a18
Giorgia <giorgialeoniac@gmail.com>
2020-03-15 21:14:41 -0300

    Updates

---
+++ precache-manifest.b60bf96173ce7d741fd5f44d3bb08981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a7e2373f921e877f25089f6b12f8a3",
+    "url": "/Talentos-Digitais-Localiza/index.html"
+  },
+  {
+    "revision": "fea45369e64f6c408090",
+    "url": "/Talentos-Digitais-Localiza/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "ffdfb30d2bd72a816128",
+    "url": "/Talentos-Digitais-Localiza/static/js/2.7817102d.chunk.js"
+  },
+  {
+    "revision": "fea45369e64f6c408090",
+    "url": "/Talentos-Digitais-Localiza/static/js/main.be8a1e13.chunk.js"
+  },
+  {
+    "revision": "b56b61aa1c23636e7f43",
+    "url": "/Talentos-Digitais-Localiza/static/js/runtime-main.50c2b543.js"
+  }
+]);
